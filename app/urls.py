28908e56e6317e91811089cb8c5bf65220ed6106
Vinícius Dias <vyvy292004@gmail.com>
2025-07-23 17:16:41 -0300

    feat: adds genres get endpoint

--- app/urls.py
+++ app/urls.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 from django.urls import path
 from django.http import JsonResponse
-
+from genres.views import genre_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('genres/', genre_view, name='genre-list'),
 ]
--- genres/admin.py
+++ genres/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from genres.models import Genre
 # Register your models here.
+
+@admin.register(Genre)
+class GenreAdmin(admin.ModelAdmin):
+    list_display = ('name', )
+    search_filds = ('name', )
