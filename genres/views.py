bd0751758f3f470f8ca3ad156091475ab1c1030a
Vinícius Dias <vyvy292004@gmail.com>
2025-07-23 18:26:39 -0300

    adds post endpoint to genres

--- genres/views.py
+++ genres/views.py
@@ -1,11 +1,21 @@
 from django.http import JsonResponse
 from genres.models import Genre
 from django.views import View
+from django.views.decorators.csrf import csrf_exempt
+from django.utils.decorators import method_decorator
+import json
 
+@method_decorator(csrf_exempt, name='dispatch')
 class GenreView(View):
-    def get(self, request, *args, **kwarg):
+    def get(self, request, *args, **kwargs):
         genres = Genre.objects.all()
         data = [{'id' : genre.id, 'name' : genre.name} for genre in genres]
         return JsonResponse(data, safe=False)
     def post(self, request, *args, **kwargs):
-        pass
+        data = json.loads(request.body.decode('utf-8'))
+        if data['name']:
+            new_genre = Genre(name=data['name'])
+            new_genre.save()
+            return JsonResponse({'id' : new_genre.id, 'name': new_genre.name}, status=201)
+        else:
+            return JsonResponse({'error' : 'bad request'}, status=400)
