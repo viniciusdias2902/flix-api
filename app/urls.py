d2d789a2da3aead24794709cbde5a108fa2a5077
Vinícius Dias <vyvy292004@gmail.com>
2025-07-23 17:22:31 -0300

    refactor: transform the genre function based view in a class based view

--- app/urls.py
+++ app/urls.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
 from django.urls import path
 from django.http import JsonResponse
-from genres.views import genre_view
+from genres.views import GenreView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('genres/', genre_view, name='genre-list'),
+    path('genres/', GenreView.as_view(), name='genre-list'),
 ]
--- genres/views.py
+++ genres/views.py
@@ -1,7 +1,11 @@
 from django.http import JsonResponse
 from genres.models import Genre
+from django.views import View
 
-def genre_view(request):
-    genres = Genre.objects.all()
-    data = [{'id' : genre.id, 'name' : genre.name} for genre in genres]
-    return JsonResponse(data, safe=False)
+class GenreView(View):
+    def get(self, request, *args, **kwarg):
+        genres = Genre.objects.all()
+        data = [{'id' : genre.id, 'name' : genre.name} for genre in genres]
+        return JsonResponse(data, safe=False)
+    def post(self, request, *args, **kwargs):
+        pass
