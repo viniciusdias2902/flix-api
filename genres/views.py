f6bf16df87fdb874d5c9e34d0df18140fa9c6016
Vinícius Dias <vyvy292004@gmail.com>
2025-07-23 18:38:49 -0300

    refactor: better error handling

--- genres/views.py
+++ genres/views.py
@@ -3,6 +3,7 @@ from genres.models import Genre
 from django.views import View
 from django.views.decorators.csrf import csrf_exempt
 from django.utils.decorators import method_decorator
+from django.shortcuts import get_object_or_404
 import json
 
 @method_decorator(csrf_exempt, name='dispatch')
@@ -23,11 +24,11 @@ class GenreListView(View):
 @method_decorator(csrf_exempt, name='dispatch')
 class GenreDetailView(View):
     def get(self, request, pk):
-        genre = Genre.objects.get(pk=pk)
-        if genre:
+        try:
+            genre = get_object_or_404(Genre, pk=pk)
             data = {'id' : genre.id, 'name' : genre.name}
             return JsonResponse(data)
-        else:
+        except Genre.DoesNotExist:
             return JsonResponse({'error' : 'id not found'}, status=404)
 
 
