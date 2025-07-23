e4bfb724aab500815563608cfdd60a073c566f80
Vinícius Dias <vyvy292004@gmail.com>
2025-07-23 16:52:18 -0300

    feat: adds the hello endpoint

---
+++ app/urls.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from django.urls import path
+from django.http import JsonResponse
+
+def hello_view(request):
+    return JsonResponse({'message' : 'Hello World'})
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('hello/', hello_view)
+]
