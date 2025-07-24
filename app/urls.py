26885ef918ca20238af56af6ccfb708d548000ae
Vinícius Dias <vyvy292004@gmail.com>
2025-07-24 16:40:45 -0300

    refactor: changes a class based view from django to a class based view of the django rest framework

--- app/urls.py
+++ app/urls.py
@@ -1,10 +1,9 @@
 from django.contrib import admin
 from django.urls import path
-from django.http import JsonResponse
-from genres.views import GenreListView, GenreDetailView
+from genres.views import GenreCreateListView, GenreDetailView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('genres/', GenreListView.as_view(), name='genre-create-list'),
+    path('genres/', GenreCreateListView.as_view(), name='genre-create-list'),
     path('genres/<int:pk>/', GenreDetailView.as_view(), name='genre-detail-update'),
 ]
--- genres/views.py
+++ genres/views.py
@@ -4,22 +4,28 @@ from django.views import View
 from django.views.decorators.csrf import csrf_exempt
 from django.utils.decorators import method_decorator
 from django.shortcuts import get_object_or_404
+from rest_framework import generics
 import json
+from genres.serializers import GenreSerializer
 
-@method_decorator(csrf_exempt, name='dispatch')
-class GenreListView(View):
-    def get(self, request, *args, **kwargs):
-        genres = Genre.objects.all()
-        data = [{'id' : genre.id, 'name' : genre.name} for genre in genres]
-        return JsonResponse(data, safe=False)
-    def post(self, request, *args, **kwargs):
-        data = json.loads(request.body.decode('utf-8'))
-        if data['name']:
-            new_genre = Genre(name=data['name'])
-            new_genre.save()
-            return JsonResponse({'id' : new_genre.id, 'name': new_genre.name}, status=201)
-        else:
-            return JsonResponse({'error' : 'bad request'}, status=400)
+class GenreCreateListView(generics.ListCreateAPIView):
+    queryset = Genre.objects.all()
+    serializer_class = GenreSerializer
+
+# @method_decorator(csrf_exempt, name='dispatch')
+# class GenreListView(View):
+#     def get(self, request, *args, **kwargs):
+#         genres = Genre.objects.all()
+#         data = [{'id' : genre.id, 'name' : genre.name} for genre in genres]
+#         return JsonResponse(data, safe=False)
+#     def post(self, request, *args, **kwargs):
+#         data = json.loads(request.body.decode('utf-8'))
+#         if data['name']:
+#             new_genre = Genre(name=data['name'])
+#             new_genre.save()
+#             return JsonResponse({'id' : new_genre.id, 'name': new_genre.name}, status=201)
+#         else:
+#             return JsonResponse({'error' : 'bad request'}, status=400)
 
 @method_decorator(csrf_exempt, name='dispatch')
 class GenreDetailView(View):
